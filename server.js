46e57b19adee75ed3033ca8d0c0ee94bfbb439ac
jgayta81 <jgayta81@gmail.com>
2026-04-05 08:56:14 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Website running on port ${PORT}`)
+})
